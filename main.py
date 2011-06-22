eb8dd418c45c43ad0ba6fc435217b8fbe611cde9
Aleksandr Motsjonov <soswow@gmail.com>
2011-06-23 00:02:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util, template
+from django.utils import simplejson
+from google.appengine.api import users
+
+from model import Vote
+
+vote_map = {"like":1,
+            "dont-like":-1,
+            "mega":100500}
+
+class VoteHandler(webapp.RequestHandler):
+    def get(self, vote):
+        if vote in vote_map:
+            Vote(score=vote_map[vote]).save()
+        self.response.out.write('OK!')
+
+class VoteAdminHandler(webapp.RequestHandler):
+    def to_json(self, votes):
+        return simplejson.dumps([vote.to_list_json() for vote in votes])
+
+    @util.login_required
+    def get(self):
+        votes = Vote.all().fetch(100)
+        self.response.out.write(self.to_json(votes))
+
+def main():
+    application = webapp.WSGIApplication([
+            (r'/do/logout', webapp.RedirectHandler.new_factory(users.create_logout_url('/index.html'))),
+            (r'/do/login', webapp.RedirectHandler.new_factory(users.create_login_url('/_ah/admin'))),
+            (r'/do/vote/(.*)', VoteHandler),
+            (r'/do/admin/votes/list', VoteAdminHandler),
+            (r'/admin/', webapp.RedirectHandler.new_factory('/admin/index.html', permanent=True)),
+            (r'.*', webapp.RedirectHandler.new_factory('/index.html', permanent=True))],
+         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
---
+++ model.py
@@ -0,0 +1,17 @@
+from time import mktime
+from google.appengine.ext import db
+#from google.appengine.api import users
+
+class Vote(db.Model):
+    score = db.IntegerProperty(required=True, default=0,choices=(1, -1, 100500))
+    created_at = db.DateTimeProperty(auto_now_add=True)
+
+    def get_created_at_tm(self):
+        return int(self.created_at.strftime("%s")) * 1000
+
+    def to_obj_json(self):
+        return {'score':self.score,
+                 'created_at': self.get_created_at_tm()}
+
+    def to_list_json(self):
+        return [self.get_created_at_tm(), self.score]
