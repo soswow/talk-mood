9adbf3e46ec172a393753f8f16b05d49427f6a8f
Anton Keks <anton@codeborne.com>
2011-06-28 20:07:32 +0300

    run updates in a separate thread

--- src/eu/devclub/talkmood/TalkMood.java
+++ src/eu/devclub/talkmood/TalkMood.java
@@ -1,13 +1,14 @@
 package eu.devclub.talkmood;
 
 import javax.swing.*;
+import javax.swing.Timer;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 public class TalkMood extends JPanel {
     public static final String URL_PREFIX = "http://talkmood.appspot.com";
@@ -49,11 +50,12 @@ public class TalkMood extends JPanel {
             }
         }).start();
 
-        new Timer(2000, new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                updateLikeCount(likeCount);
-            }
-        }).start();
+        new java.util.Timer(true).scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        updateLikeCount(likeCount);
+                    }
+                }, 0, 2000);
     }
 
     private void addPopupMenu(Container frame) {
@@ -88,14 +90,19 @@ public class TalkMood extends JPanel {
         timeLabel.setText(simpleDateFormat.format(new Date()));
     }
 
-    private void updateLikeCount(JLabel likeCount) {
+    private void updateLikeCount(final JLabel likeCount) {
         try {
             URL url = new URL(URL_PREFIX + "/do/vote/num/from/" + showEventsSince);
             BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
             String votes = reader.readLine();
             if (votes == null || votes.trim().isEmpty()) votes = "?";
-            likeCount.setText(votes);
             reader.close();
+            final String finalVotes = votes;
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    likeCount.setText(finalVotes);
+                }
+            });
         }
         catch (Exception e) {
             e.printStackTrace();
